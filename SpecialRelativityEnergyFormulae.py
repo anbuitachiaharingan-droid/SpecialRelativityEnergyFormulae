5177cd58cedd6d4e205f85b7ad4f0704eb4110e3
anbuitachiaharingan-droid <anbuitachiaharingan@gmail.com>
2026-09-12 07:54:10 -0400

    Create SpecialRelativityEnergyFormulae.py

---
+++ SpecialRelativityEnergyFormulae.py
@@ -0,0 +1,23 @@
+# Define zero as an indefinite number or a cipher. Given ! = sigmasummation from -infinity to capital Beta for the series kcsubinitial*asub(i*x+y*j+kz) + 1), what is absolute zero-valued energy, or ceroes absolutos?
+
+## Using special relativity zero is both bolstered by slavery and classic black/Juadeic freedom. It is quite observant/observatory.
+
+### (Energy)**3 = (Energy)*(Energy)*(Energy);
+
+def special_relativity(m, c_squared):
+    m = 1*(((9.11)*(10**(-31))));
+    m_knought = m;
+    m_final = (1 - m_knought);
+    c = (300000); 
+    c_squared = (300000)**2;E_initial = (m*(c)**2);E_knought = ((m_knought)*(c)**2);
+    E_final = ((1 - m_final)*(c**2));
+    
+    print(str(int(E_knought)) + str("Joules"));
+    
+    print(str(int(E_knought)) + str("Joules"));
+    
+    print(str(int(E_final)) + str("Joules"));
+
+m = 1*(((9.11)*(10**(-31))));
+c_squared = (300000);
+special_relativity(m, c_squared);
